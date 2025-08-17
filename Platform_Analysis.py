90f703c9e4459ff7956e6ce8dc5938e2d7dae98f
Aaron Chen-Xiao <aaroncx6@gmail.com>
2025-08-17 09:36:12 -0700

    first commit

---
+++ Platform_Analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+netflix = pd.read_csv("netflix_titles.csv")
+disney = pd.read_csv("disney_plus_titles.csv")
+hulu = pd.read_csv("hulu_titles.csv")
+amazon = pd.read_csv("amazon_prime_titles.csv")
